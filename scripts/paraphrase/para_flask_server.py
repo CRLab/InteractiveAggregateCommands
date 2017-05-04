80d1ad02476bcb627d349857bc8336bf43e6bce4
David Watkins <djw2146@columbia.edu>
2017-05-04 12:20:50 -0400

    Changed port to integer

--- scripts/paraphrase/para_flask_server.py
+++ scripts/paraphrase/para_flask_server.py
@@ -17,60 +17,67 @@ paraphraser = Paraphrase_detector(encoder)
 
 app = Flask(__name__)
 
+
 # use this to detect paraphrases
-@app.route('/paraphrase_this', methods = ['GET', 'POST'])
+@app.route('/paraphrase_this', methods=['GET', 'POST'])
 def return_closest_sentence():
-	#import IPython
-	#IPython.embed()
-	sentence = request.json['data']
-	closest_sentence = paraphraser.check_task_list_for_paraphrases(sentence)
-	return jsonify({"data" : closest_sentence})
+    # import IPython
+    # IPython.embed()
+    sentence = request.json['data']
+    closest_sentence = paraphraser.check_task_list_for_paraphrases(sentence)
+    return jsonify({"data": closest_sentence})
+
 
 # use this to add a pose task
-@app.route('/add_pose_task', methods = ['GET', 'POST'])
+@app.route('/add_pose_task', methods=['GET', 'POST'])
 def add_pose_task():
-	pose_string = request.json['data']
-	paraphraser.add_sentence('pose', pose_string)
-	return "ok"
+    pose_string = request.json['data']
+    paraphraser.add_sentence('pose', pose_string)
+    return "ok"
+
 
 # use this to add an object task (e.g. grasp an object)
-@app.route('/add_object_task', methods = ['GET', 'POST'])
+@app.route('/add_object_task', methods=['GET', 'POST'])
 def add_object_task():
-	object_string = request.json['data']
-	paraphraser.add_sentence('object', object_string)
-	return "ok"
+    object_string = request.json['data']
+    paraphraser.add_sentence('object', object_string)
+    return "ok"
+
 
 # use this to add a place task (e.g. go to a place)
-@app.route('/add_place_task', methods = ['GET', 'POST'])
+@app.route('/add_place_task', methods=['GET', 'POST'])
 def add_place_task():
-	place_string = request.json['data']
-	paraphraser.add_sentence('place', place_string)
-	return "ok"
+    place_string = request.json['data']
+    paraphraser.add_sentence('place', place_string)
+    return "ok"
+
 
 # use this to add poses, objects, and places to commands
 # known to the paraphraser. Useful for testing 
 # Modify the underlying paraphraser method to change
 # objects, poses, and places added.
-@app.route('/start_test_env', methods = ['GET', 'POST'])
+@app.route('/start_test_env', methods=['GET', 'POST'])
 def start_test_env():
-	paraphraser.instantiate_test_env()
-	return "ok"
+    paraphraser.instantiate_test_env()
+    return "ok"
+
 
 # Add an entirely new command to the list known to the paraphraser
 # Do not use for object, pose, or place commands
-@app.route('/add_new_command', methods = ['GET', 'POST'])
+@app.route('/add_new_command', methods=['GET', 'POST'])
 def add_new_command():
-	new_command = request.json['data']
-	paraphraser.add_command(new_command)
-	return "ok"
+    new_command = request.json['data']
+    paraphraser.add_command(new_command)
+    return "ok"
+
 
 # Reset the paraphraser so that it forgets all the poses, objects,
 # places, and new commands that have been given to it
-@app.route('/reset', methods = ['GET', 'POST'])
+@app.route('/reset', methods=['GET', 'POST'])
 def reset():
-	paraphraser = Paraphrase_detector(encoder)
-	return "ok"
+    paraphraser = Paraphrase_detector(encoder)
+    return "ok"
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
