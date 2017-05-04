484caafae6dbfbb710e9d49a829ddbec7e26df82
David Watkins <djw2146@columbia.edu>
2017-05-04 16:23:52 -0400

    Added function stubs for generic robot interface

--- scripts/interfaces/generic_interface.py
+++ scripts/interfaces/generic_interface.py
@@ -19,3 +19,34 @@ class GenericInterface:
 
     def recognize_object(self):
         pass
+
+    def moveTo(self, location):
+        pass
+
+    def followMe(self):
+        pass
+
+    def graspObject(self, object_name):
+        pass
+
+    def placeObject(self, object_name):
+        pass
+
+    def askForObject(self):
+        pass
+
+    def enactPose(self, pose):
+        pass
+
+    def recognize_object(self):
+        pass
+
+    def getCurrentLocation(self):
+        pass
+
+    def cartesianTransform(self, direction, meters):
+        pass
+
+    def getCurrentPose(self):
+        pass
+
