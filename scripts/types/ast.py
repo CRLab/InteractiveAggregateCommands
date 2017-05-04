93eee5e21f0033b2012b020910ed4c1979a6c6be
David Watkins <djw2146@columbia.edu>
2017-05-04 13:22:39 -0400

    Error with record print method

--- scripts/types/ast.py
+++ scripts/types/ast.py
@@ -45,7 +45,7 @@ class Record(Command):
         self.task_list = task_list
 
     def __str__(self):
-        return "Record:{\nid: {},\ntask_list: {}}\n".format(self.command_id, self.task_list)
+        return "Record:{\n id: {},\n task_list: {}}\n".format(self.command_id, self.task_list)
 
     def pretty_print(self):
         return "record {} as {}".format(self.command_id, self.task_list.pretty_print())
